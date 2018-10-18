78d0e3ee78c12bb5bd66a24b37a35c2faccf4daf
epauser <epauser@localhost.localdomain>
2018-10-18 10:22:34 +0100

    new script

---
+++ Week4/improved.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cnt=$(grep processor /proc/cpuinfo | wc -l)
+
+# this is is not quite right
+# find what test to use for empty string ($1)
+#
+if [ $1 ]; then
+  echo "Please provide CPU parameter"
+  exit 9
+fi
+
+if [ $cnt -lt $1 ]; then
+  echo "Insufficient CPUs, install abandoned"
+fi
